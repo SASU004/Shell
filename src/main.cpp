d51f6257b40520f0b18b505381e685bf902c4587
SASU004 <shaswataghosh091@gmail.com>
2026-02-17 03:24:18 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define endl '\n'
 
 int main() {
     cout << unitbuf;
     cerr << unitbuf;
+    string s;
+    
+    while (true){
 
     cout << "$ ";
-    string s;
-    while(getline(cin,s)){
+    getline(cin,s);
     if(s=="exit")break;
-    if(s.substr(0,5)=="echo "){
+    if(s.substr(0,5)=="echo ")
       cout<<s.substr(5)<<'\n';
-      cout<<"$ ";
+    else if(s.substr(0,5)=="type "){
+      if(s.substr(5,9)=="echo")
+      cout<<"echo "<<"is a shell builtin"<<endl;
+      else if(s.substr(5,9)=="exit")
+      cout<<"exit "<<"is a shell builtin"<<endl;
+      else if(s.substr(5,9)=="type")
+      cout<<"type "<<"is a shell builtin"<<endl;
+      else cout<<s.substr(5,9)<<": not found"<<endl;
+     
     }
-
-    else {
-    cout << s << ": command not found" << '\n';
-    cout<<"$ " ;
-    }
+    else cout << s << ": command not found" << '\n';
+    
 
   }
 }
+
