63ca99ed939beaf2e3270c25cbd8bf4ad5f2fdc0
SASU004 <shaswataghosh091@gmail.com>
2026-02-17 01:30:52 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main() {
     cout << unitbuf;
     cerr << unitbuf;
 
-    cout << "$ ";
+    cout << "$";
     string s;
     while(getline(cin,s)){
     cout << s << ": command not found" << '\n';
