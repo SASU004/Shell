531e7baf5391963ccaffd1c8971a927244f96b76
SASU004 <shaswataghosh091@gmail.com>
2026-02-17 02:57:25 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,11 @@ int main() {
     string s;
     while(getline(cin,s)){
     if(s=="exit")break;
-    if(s.substr(0,5)=="echo ");
+    if(s.substr(0,5)=="echo "){
+      cout<<s.substr(5)<<'\n';
+      cout<<"$ ";
+    }
+
     else {
     cout << s << ": command not found" << '\n';
     cout<<"$ " ;
