6c6d1ee4fc66047e3f21d469451bfd6d586a0b0a
Shaswata Ghosh <shaswataghosh091@gmail.com>
2026-03-28 03:22:51 +0530

    added the echo handler function
    basically if we use echo then argument it just prints the argument

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-
+bool handle_echo(const string& input){
+if(input=="echo"){
+  cout << "write something :)\n";
+  return true ;
+}
+if(input.substr(0,5)=="echo "){
+  cout << input.substr(5) <<endl;
+  return true ;
+}
+return false ;
+}
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  
+  cout << unitbuf;
+  cerr << unitbuf;
   while (true ){
     cout<< "$ ";
     string input;
     getline(cin,input);
     if(input=="exit")break;
+    if(handle_echo(input))continue;
     cout<< input << ": command not found"<< endl;
   }
 }
