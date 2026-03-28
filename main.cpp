d029cf308a7aaef2a7ade9d339ad92ec24df319d
Shaswata Ghosh <shaswataghosh091@gmail.com>
2026-03-29 03:08:46 +0530

    added built in handle type

--- main.cpp
+++ main.cpp
@@ -2,16 +2,36 @@
 using namespace std;
  
 bool handle_echo(const string& input){
-if(input=="echo"){
-  cout << "write something :)\n";
-  return true ;
+  if(input=="echo"||input=="echo "){
+    cout << "write something :)\n";
+    return true ;
+  }
+  if(input.substr(0,5)=="echo "){
+    cout << input.substr(5) <<endl;
+    return true ;
+  }
+  return false ;
 }
-if(input.substr(0,5)=="echo "){
-  cout << input.substr(5) <<endl;
-  return true ;
+
+bool handle_type(const string& input){
+  if(input=="type"||input=="type "){
+    cout << "write something :)\n";
+    return true ;
+  }
+  if(input.substr(0,5)=="type "){
+    string cmd = input.substr(5);
+    if(cmd=="type")
+      cout<<"type is a shell builtin"<<endl;
+    else if(cmd=="exit")
+      cout<<"exit is a shell builtin"<<endl;
+    else if(cmd=="echo")
+      cout<<"echo is a shell builtin"<<endl;
+    else cout<<cmd<<": not found"<<endl;
+      return true;
+    }
+  return false;
 }
-return false ;
-}
+
 int main() {
   
   cout << unitbuf;
@@ -22,6 +42,7 @@ int main() {
     getline(cin,input);
     if(input=="exit")break;
     if(handle_echo(input))continue;
+    if(handle_type(input))continue;
     cout<< input << ": command not found"<< endl;
   }
 }
